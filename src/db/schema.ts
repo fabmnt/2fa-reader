7c566f3160e19b6f0848b38df2e1b314134e2935
Fabián Montoya <fabianmontoya2802@gmail.com>
2025-11-03 21:09:49 -0600

    Add drizzle-zod dependency and update database schema for matches; remove obsolete matches data

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,17 @@
-import { integer, pgTable, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
 	name: text().notNull(),
 	age: integer().notNull(),
 	email: text().notNull().unique(),
+});
+
+export const matchesTable = pgTable("matches", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	provider: text().notNull(),
+	subject: text().notNull(),
+	body: text().notNull(),
+	createdAt: timestamp().notNull().defaultNow(),
+	updatedAt: timestamp().notNull().defaultNow(),
 });
